ffcf01ced4d3a932d00e825b180d0066a3b2a8a5
dekaden|Z <github.dekadenz@xoxy.net>
2013-10-09 12:57:03 +0200

    Complete data scheme overhaul
     - individual tags instead of concatenated ones for each picture
     - Tags are associated to the tagging user.
     - Votes are associated to the voting user.
     - Comments can be voted up or down.
     - Images, comments, and tags can be marked deleted with a reason.
     - Prepare for password hashs produced by password_hash().
     - Store access tokens and image hashs as binary strings.
     - Store timestamps as BIGINT because other possibilities of MySQL
       and MariaDB suck or offer no real advantage over BIGINT.
     - Legacy user score and image ratings can be retained.
     - Lots of views do demonstrate and/or facilitate access to aggregated
       data
     - Abuse notification managment for things like spam, porn, copyright
       violations, or generally unethical or illegal content
    
    I also included a crude migration script for old data as seen on Lowbird.

---
+++ migrate-lowbird.sql
@@ -0,0 +1,51 @@
+INSERT INTO `pl_users`
+	(`id`, `registered`, `name`, `valid`, `admin`, `avatar`, `website`, `email`)
+SELECT `id`, UNIX_TIMESTAMP(`registered`), `name`,
+	`valid` <> 0, `admin` <> 0,
+	NULLIF(`avatar`, 'data/avatars/default.png'),
+	NULLIF(`website`, ''),
+	NULLIF(`email`, '')
+FROM `lowbird`.`lb_users`;
+
+INSERT INTO `pl_users_legacy`
+SELECT `id`, `score`
+FROM `lowbird`.`lb_users`
+WHERE `score` IS NOT NULL AND `score` <> 0;
+
+
+INSERT INTO `pl_images`
+	(`id`, `logged`, `user`, `keyword`, `image`, `thumb`, `source`)
+SELECT i.`id`, UNIX_TIMESTAMP(i.`logged`), i.`user`, i.`keyword`, i.`image`, i.`thumb`, NULLIF(i.`source`, '')
+FROM `pl_users` AS u INNER JOIN `lowbird`.`lb_images` AS i
+ON i.`user` = u.`id`;
+
+INSERT INTO `pl_images_legacy`
+SELECT old.`id`, GREATEST(old.`votes`, 0), GREATEST(old.`score`, 0), NULLIF(old.`tags`, '')
+FROM `pl_images` AS new INNER JOIN `lowbird`.`lb_images` AS old
+ON new.`id` = old.`id`
+WHERE old.`votes` > 0 OR (old.`tags` IS NOT NULL AND old.`tags` <> '');
+
+
+INSERT INTO `pl_comments`
+	(`id`, `imageId`, `userId`, `created`, `content`)
+SELECT c.`id`, c.`imageId`, c.`userId`, UNIX_TIMESTAMP(c.`created`), c.`content`
+FROM `pl_images` AS i INNER JOIN `lowbird`.`lb_comments` AS c
+ON i.`id` = c.`imageId`;
+
+
+INSERT INTO `pl_imagecolors`
+	(`imageId`, `value`, `saturation`, `hue`)
+SELECT `imageId`,
+	(@max := GREATEST(r, g, b)) / 255 AS `value`,
+	IF(@max <> 0, (@max - (@min := LEAST(r, g, b))) / @max, 0) AS `saturation`,
+	(@hue := (CASE @max
+		WHEN @min THEN 0
+		WHEN r THEN 0 + (CAST(g AS SIGNED) - CAST(b AS SIGNED)) / (@max - @min)
+		WHEN g THEN 2 + (CAST(b AS SIGNED) - CAST(r AS SIGNED)) / (@max - @min)
+		WHEN b THEN 4 + (CAST(r AS SIGNED) - CAST(g AS SIGNED)) / (@max - @min)
+	END) / 6) + (@hue < 0) AS `hue`
+FROM `pl_images` AS i INNER JOIN `lowbird`.`lb_imagecolors`
+ON i.`id` = `imageId`;
+
+
+INSERT INTO `pl_tags_legacy`
--- picturelicious.sql
+++ picturelicious.sql
@@ -1,85 +1,235 @@
-CREATE TABLE `pl_comments` (
-  `id` int(11) NOT NULL auto_increment,
-  `imageId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `content` text collate utf8_unicode_ci NOT NULL,
+CREATE TABLE `pl_users` (
+  `id` SERIAL,
+  `registered` BIGINT NOT NULL,
+  `name` VARCHAR(63) NOT NULL,
+  `pass` VARBINARY(255),
+  `valid` BOOL NOT NULL,
+  `remember` BINARY(16),
+  `admin` BOOL NOT NULL DEFAULT FALSE,
+  `avatar` VARCHAR(255),
+  `website` VARCHAR(1023),
+  `email` VARCHAR(255),
   PRIMARY KEY  (`id`),
-  KEY `imageId` (`imageId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `name` (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
-CREATE TABLE `pl_imagecolors` (
-  `id` int(11) NOT NULL auto_increment,
-  `imageId` int(11) NOT NULL,
-  `r` SMALLINT(3) NOT NULL default '0',
-  `g` SMALLINT(3) NOT NULL default '0',
-  `b` SMALLINT(3) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# Legacy user scores
+CREATE TABLE `pl_users_legacy` (
+  `user` SERIAL,
+  `score` BIGINT NOT NULL,
+  PRIMARY KEY (`user`),
+  FOREIGN KEY (`user`) REFERENCES `pl_users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 
 CREATE TABLE `pl_images` (
-  `id` int(11) NOT NULL auto_increment,
-  `logged` datetime NOT NULL,
-  `user` int(11) NOT NULL,
-  `score` float NOT NULL default '0',
-  `votes` int(11) NOT NULL default '0',
-  `keyword` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `tags` text collate utf8_unicode_ci NOT NULL,
-  `image` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `thumb` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `hash` char(32) collate utf8_unicode_ci NOT NULL,
-  `source` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `id` SERIAL,
+  `logged` BIGINT NOT NULL,
+  `user` BIGINT UNSIGNED NOT NULL,
+  `keyword` VARCHAR(255) NOT NULL,
+  `image` VARCHAR(255) NOT NULL,
+  `thumb` VARCHAR(255) NOT NULL,
+  `hash` BINARY(20) NOT NULL,
+  `source` VARCHAR(1023),
+  `delete_reason` ENUM('', 'other', 'repost', 'low quality', 'copyright', 'spam', 'hardcore porn/gore', 'illegal') NOT NULL DEFAULT '',
+  `width` INT UNSIGNED,
+  `height` INT UNSIGNED,
   PRIMARY KEY  (`id`),
-  KEY `keyword` (`keyword`),
-  FULLTEXT KEY `tags` (`tags`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  FOREIGN KEY (`user`) REFERENCES `pl_users` (`id`),
+  INDEX `user` (`user`, `logged` DESC) USING BTREE,
+  INDEX `keyword` (`keyword`) USING HASH,
+  INDEX `hash` (`hash`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE VIEW `plv_uploadlimit` AS
+SELECT `user`, IF(COUNT(`logged`) >= 10, UNIX_TIMESTAMP() - MIN(`logged`) + 2*3600, 0) AS next_upload_time
+FROM `pl_images`
+WHERE `logged` > UNIX_TIMESTAMP() - 2*3600
+GROUP BY `user`;
 
 
-CREATE TABLE `pl_iplock` (
-  `ip` int(11) NOT NULL,
-  `imageId` int(11) NOT NULL,
-  `ts` int(11) NOT NULL,
-  PRIMARY KEY  (`ip`,`imageId`),
-  KEY `ts` (`ts`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# Legacy image ratings and tags
+CREATE TABLE `pl_images_legacy` (
+  `image` SERIAL,
+  `votecount` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `rating` DOUBLE NOT NULL DEFAULT 0,
+  `tags` TEXT,
+  PRIMARY KEY (`image`),
+  FOREIGN KEY (`image`) REFERENCES `pl_images` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
+CREATE TABLE pl_imageratings (
+  `image` BIGINT UNSIGNED NOT NULL,
+  `user` BIGINT UNSIGNED NOT NULL,
+  `rating` TINYINT NOT NULL,
+  PRIMARY KEY (`image`, `user`),
+  FOREIGN KEY (`image`) REFERENCES `pl_images` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`user`) REFERENCES `pl_users` (`id`),
+  INDEX `user` (`user`) USING HASH
+) ENGINE=InnoDB;
+
+
+# Image ratings excluding votes by respective owners, including legacy ratings, aggregated by image
+CREATE VIEW `plv_images` AS
+SELECT i.*,
+  COUNT(r.`rating`) + IFNULL(l.`votecount`, 0) AS `votecount`,
+  (IFNULL(SUM(r.`rating`), 0) + IFNULL(l.`rating`, 0) * IFNULL(l.`votecount`, 0)) / (COUNT(r.`rating`) + IFNULL(l.`votecount`, 0)) AS `rating`
+FROM `pl_images` AS i
+  LEFT OUTER JOIN `pl_imageratings` AS r ON i.`id` = r.`image`
+  LEFT OUTER JOIN `pl_images_legacy` AS l ON i.`id` = l.`image`
+WHERE r.`user` IS NULL OR i.`id` <> r.`user`
+GROUP BY i.`id`;
+
+
+CREATE TABLE `pl_comments` (
+  `id` SERIAL,
+  `imageId` BIGINT UNSIGNED NOT NULL,
+  `userId` BIGINT UNSIGNED NOT NULL,
+  `created` BIGINT NOT NULL,
+  `content` TEXT NOT NULL,
+  `delete_reason` ENUM('', 'other', 'spam', 'illegal') NOT NULL DEFAULT '',
+  PRIMARY KEY  (`id`),
+  FOREIGN KEY (`userId`) REFERENCES `pl_users` (`id`),
+  FOREIGN KEY (`imageId`) REFERENCES `pl_images` (`id`) ON DELETE CASCADE,
+  INDEX `image` (`imageId`) USING HASH,
+  INDEX `user` (`userId`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE `pl_commentratings` (
+  `comment` BIGINT UNSIGNED NOT NULL,
+  `user` BIGINT UNSIGNED NOT NULL,
+  `rating` TINYINT NOT NULL,
+  PRIMARY KEY (`comment`, `user`),
+  FOREIGN KEY (`user`) REFERENCES `pl_users` (`id`),
+  FOREIGN KEY (`comment`) REFERENCES `pl_comments` (`id`) ON DELETE CASCADE,
+  INDEX `user` (`user`) USING HASH
+) ENGINE=InnoDB;
+
+
+# Up and down votes excluding respective authors, aggregated by comment
+CREATE VIEW `plv_comments` AS
+SELECT c.*, SIGN(r.`rating`) AS `rating_type`, COUNT(r.`rating`) as `count`
+FROM `pl_comments` AS c LEFT OUTER JOIN `pl_commentratings` AS r
+ON c.`id` = r.`comment`
+WHERE c.`userId` <> r.`user`
+GROUP BY c.`id`, `rating_type`;
+
+
+CREATE TABLE `pl_imagecolors` (
+  `imageId` BIGINT UNSIGNED NOT NULL,
+  `hue` FLOAT UNSIGNED NOT NULL,
+  `saturation` FLOAT UNSIGNED NOT NULL,
+  `value` FLOAT UNSIGNED NOT NULL,
+  FOREIGN KEY (`imageId`) REFERENCES `pl_images` (`id`) ON DELETE CASCADE,
+  INDEX `image` (`imageId`) USING HASH
+) ENGINE=InnoDB;
+
+
+CREATE TABLE `pl_tags` (
+  `image` BIGINT UNSIGNED NOT NULL,
+  `tag` VARCHAR(127) NOT NULL,
+  `author` BIGINT UNSIGNED,
+  `time` BIGINT,
+  `delete_reason` ENUM('', 'other', 'spam') NOT NULL DEFAULT '',
+  PRIMARY KEY (`image`, `tag`),
+  FOREIGN KEY (`author`) REFERENCES `pl_users` (`id`) ON DELETE SET NULL,
+  FOREIGN KEY (`image`) REFERENCES `pl_images` (`id`) ON DELETE CASCADE,
+  INDEX `tag` (`tag`) USING BTREE,
+  INDEX `author` (`author`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+# Log of "quick tag" actions to prevent repetition
 CREATE TABLE `pl_taglog` (
-  `id` int(11) NOT NULL auto_increment,
-  `tagged` datetime NOT NULL,
-  `userId` int(11) NOT NULL,
-  `imageId` int(11) NOT NULL,
-  `locked` tinyint(4) NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `tagged` (`tagged`),
-  KEY `userId` (`userId`),
-  KEY `imageId` (`imageId`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `tagged` BIGINT NOT NULL,
+  `userId` BIGINT UNSIGNED NOT NULL,
+  `imageId` BIGINT UNSIGNED NOT NULL,
+  `locked` BOOL NOT NULL,
+  PRIMARY KEY  (`userId`, `imageId`),
+  FOREIGN KEY (`userId`) REFERENCES `pl_users` (`id`) ON DELETE CASCADE,
+  FOREIGN KEY (`imageId`) REFERENCES `pl_images` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 
-CREATE TABLE `pl_uploadlock` (
-  `id` int(11) NOT NULL auto_increment,
-  `ip` int(11) NOT NULL,
-  `ts` int(11) NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `ip` (`ip`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# Image tags sorted by overall frequency
+CREATE VIEW `pl_tag_frequency` AS
+SELECT `tag`, COUNT(`image`) AS `frequency`
+FROM `pl_tags`
+WHERE `delete_reason` = ''
+GROUP BY `tag`;
 
 
-CREATE TABLE `pl_users` (
-  `id` int(11) NOT NULL auto_increment,
-  `registered` datetime NOT NULL,
-  `name` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `pass` char(32) collate utf8_unicode_ci NOT NULL,
-  `valid` tinyint(4) NOT NULL,
-  `remember` char(32) collate utf8_unicode_ci NOT NULL,
-  `admin` tinyint(4) NOT NULL default '0',
-  `score` int(11) NOT NULL,
-  `images` int(11) NOT NULL default '0',
-  `avatar` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `website` varchar(255) collate utf8_unicode_ci NOT NULL,
-  `email` varchar(255) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# User scores from posting images, aggregated by user
+CREATE VIEW `plv_score_user_images` AS
+SELECT `user`, SUM(`delete_reason` = '') AS `count`,
+  SUM(IF(`delete_reason` = '', 10,
+    IF(CAST(`delete_reason` AS UNSIGNED) >= 6, -50, 0))) AS `score`
+FROM `pl_images`
+GROUP BY `user`;
+
+# User scores from rating images, aggregated by user
+CREATE VIEW `plv_score_user_imagevotes` AS
+SELECT `user`, COUNT(`image`) AS `count`, COUNT(`image`) * 1 AS `score`
+FROM `pl_imageratings`
+GROUP BY `user`;
+
+# User scores from image ratings by other users, aggregated by user
+CREATE VIEW `plv_score_user_imageratings` AS
+SELECT i.`user` AS `user`, (AVG(r.`rating`) - 2) * 1 AS `score`
+FROM `pl_images` AS i INNER JOIN `pl_imageratings` AS r
+ON i.`id` = r.`image`
+WHERE i.`user` <> r.`user` AND `delete_reason` = ''
+GROUP BY `user`;
+
+# User scores from comment ratings by other users, aggregated by user
+CREATE VIEW `plv_score_user_comments` AS
+SELECT r.`user` AS `user`, COUNT(`rating`) AS `count`, SUM(`rating`) * 1 AS `score`
+FROM `pl_commentratings` AS r INNER JOIN `pl_comments` AS c
+ON c.`id` = r.`comment`
+WHERE r.`user` <> c.`userId` AND `delete_reason` = ''
+GROUP BY `user`;
+
+# Support view for subquery in `plv_score_user_tags`
+CREATE VIEW `plv_score_user_image_tags` AS
+SELECT `author` AS `user`,
+  SUM(`delete_reason` = '') AS `count`,
+  SUM(IF(`delete_reason` = '', CHAR_LENGTH(`tag`), 0)) AS `length`,
+  GREATEST(LEAST(SUM(
+      IF(`delete_reason` = '', 2.5,
+        IF(CAST(`delete_reason` AS UNSIGNED) > 1, -5, 0))
+    ), 20), -5) AS `score`
+FROM `pl_tags`
+WHERE `author` IS NOT NULL
+GROUP BY `user`, `image`;
+
+# User score from tagging images, aggregated by user, limited to the interval [-5, 20] per image and user
+CREATE VIEW `plv_score_user_tags` AS
+SELECT `user`, SUM(`count`) AS `count`, SUM(`length`) AS `length`, SUM(`score`) AS `score`
+FROM `plv_score_user_image_tags`
+GROUP BY `user`;
+
+# Combined user score, aggregated by user
+CREATE VIEW `plv_score_user` AS
+SELECT u.`id`,
+  IFNULL(l.`score`, 0) + IFNULL(i.`score`, 0) + IFNULL(iv.`score`, 0) + IFNULL(ir.`score`, 0) + IFNULL(c.`score`, 0) + IFNULL(t.`score`, 0) AS `score`
+FROM `pl_users` AS u
+  LEFT OUTER JOIN `pl_users_legacy` AS l ON u.`id` = l.`user`
+  LEFT OUTER JOIN `plv_score_user_images` AS i ON u.`id` = i.`user`
+  LEFT OUTER JOIN `plv_score_user_imagevotes` AS iv ON u.`id` = iv.`user`
+  LEFT OUTER JOIN `plv_score_user_imageratings` AS ir ON u.`id` = ir.`user`
+  LEFT OUTER JOIN `plv_score_user_comments` AS c ON u.`id` = c.`user`
+  LEFT OUTER JOIN `plv_score_user_tags` AS t ON u.`id` = t.`user`;
+
+
+CREATE TABLE `pl_absuses` (
+  `type` ENUM('user', 'image', 'comment', 'tag') NOT NULL,
+  `abusedId` BIGINT UNSIGNED NOT NULL,
+  `author` BIGINT UNSIGNED,
+  `ctime` BIGINT NOT NULL,
+  `reason` VARCHAR(255) NOT NULL,
+  `comment` TEXT DEFAULT NULL,
+  PRIMARY KEY (`type`, `abusedId`, `author`)
+  #FOREIGN KEY (`author`) REFERENCES `pl_users` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
