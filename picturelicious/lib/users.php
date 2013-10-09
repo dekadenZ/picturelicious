27396d089dccf32f2784b5725cec6a5db46160e6
dekaden|Z <github.dekadenz@xoxy.net>
2013-10-09 12:51:15 +0200

    Migrate login to new DB scheme and passwords
     - Delete lots of obsolete methods.
     - Refactor large spaghetti methods into multiple smaller ones.

--- picturelicious/lib/users.php
+++ picturelicious/lib/users.php
@@ -1,28 +1,79 @@
 <?php
-
-/* 
-  An object of this class represents a user on the site. All user 
-  related tasks (registration, login, profile etc.) are handled here.
-
-  A user object is created and attempted to login on each page load.
-*/
-
 require_once( 'lib/config.php' );
 require_once( 'lib/db.php' );
 
-class User {
-  public $name = '';
-  public $id = 0;
-  public $validationString = '';
-  public $admin = 0;
-  public $website = 0;
+/*
+ * An object of this class represents a user on the site. All user
+ * related tasks (registration, login, profile etc.) are handled here.
+ *
+ * A user object is created and attempted to login on each page load.
+ */
+class User
+{
+  public $name;
+  public $id;
+  public $admin;
+  public $website;
+  public $email;
+
+  private $validationString = null;
+  private $passwordHash = null;
+
 
   public function __construct() {
+    $this->reset();
   }
 
+
+  public function __set( $prop, $value )
+  {
+    switch ($prop) {
+      case 'passwordHash':
+      case 'validationString':
+        $this->{$prop} = $value;
+        break;
+
+      default:
+        throw new Exception("Unknown or inaccessible property $prop on object of class ".__CLASS__);
+    }
+  }
+
+
+  const PROPERTIES = 'name|id|admin|website|email';
+
+  public function __from_array( $a )
+  {
+    foreach (explode('|', self::PROPERTIES) as $prop) {
+      if (isset($a[$prop]))
+        $this->{$prop} = $a[$prop];
+    }
+  }
+
+  public function &__to_array( &$a = array() )
+  {
+    foreach (explode('|', self::PROPERTIES) as $prop) {
+      if (!is_null($this->{$prop}))
+        $a[$prop] = $this->{$prop};
+    }
+    return $a;
+  }
+
+
+  public function reset()
+  {
+    $this->name = null;
+    $this->id = null;
+    $this->admin = null;
+    $this->website = null;
+    $this->email = null;
+    $this->validationString = null;
+    $this->passwordHash = null;
+  }
+
+
   public function validate( $id ) {
-    $u = DB::getRow( 
-      'SELECT id, name, admin, website FROM '.TABLE_USERS.' WHERE valid = 0 AND remember = :1', 
+    $u = DB::getRow(
+      'SELECT id, name, admin, website FROM '.TABLE_USERS.' WHERE valid = 0 AND remember = :1',
       $id
     );
 
@@ -41,7 +92,7 @@ class User {
 
     setcookie( Config::$rememberCookie, $id, time() + 3600 * 24 * 365, Config::$absolutePath );
 
-    if( Config::$vbbIntegration['enabled'] ) { 
+    if( Config::$vbbIntegration['enabled'] ) {
       global $vbulletin;
       $forum = new ForumOps($vbulletin);
       $forum->login(array('username' => $u['name']));
@@ -49,102 +100,172 @@ class User {
     return true;
   }
 
-  public function isSpamLocked() {
-    // delete all ips older than IP_LOCK_TIME
-    DB::query( 'DELETE FROM '.TABLE_UPLOADLOCK.' WHERE ts < :1', time() - Config::$uploadLockTime );
 
-    list(, $ip) = unpack('l',pack('l',ip2long($_SERVER['REMOTE_ADDR'])));
-    $r = DB::getRow( 'SELECT COUNT(*) AS c FROM '.TABLE_UPLOADLOCK.' 
-      WHERE ip = :1', 
-      $ip
-    );
-
-    if( $r['c'] > Config::$maxNumUploads ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  public function logUpload() {
-    list(, $ip) = unpack('l',pack('l',ip2long($_SERVER['REMOTE_ADDR'])));
-    DB::insertRow( TABLE_UPLOADLOCK, array('ip' => $ip, 'ts' => time()) );
-  }
-
-  public function login() {
-
-    // attempt to login via post, session or remeber cookie
+  public function login()
+  {
+    // attempt to login via post, session or remember cookie
     session_name( Config::$sessionCookie );
 
-    if( isset($_POST['login']) ) { // post login
-      $u = DB::getRow(
-        'SELECT id, name, admin, website FROM '.TABLE_USERS.' WHERE name = :1 AND pass = :2 and valid = 1', 
-        $_POST['name'], md5($_POST['pass']) 
-      );
-      if( !empty($u) ) {
-        session_start();
-        $_SESSION['id'] = $this->id = $u['id'];
-        $_SESSION['name'] = $this->name = $u['name'];
-        $_SESSION['admin'] = $this->admin = $u['admin'];
-        $_SESSION['website'] = $this->website = $u['website'];
-
-        if( !empty($_POST['remember']) ) {
-          $r = md5(uniqid(rand()));
-          DB::updateRow( TABLE_USERS, array('id' => $this->id), array('remember' => $r ) );
-          setcookie( Config::$rememberCookie, $r, time() + 3600 * 24 * 365, Config::$absolutePath );
-        }
-
-        if( Config::$vbbIntegration['enabled'] ) { 
-          global $vbulletin;
-          $forum = new ForumOps($vbulletin);
-          $forum->login(array('username' => $u['name']));
-        }
-      }
-    }
-
-    else if( !empty($_COOKIE[Config::$sessionCookie]) ) { // session running
-      session_start();
-      if( empty($_SESSION['id']) ) {
-        setcookie(Config::$sessionCookie, false, time() - 3600, Config::$absolutePath );
-      } else {
-        $this->id = $_SESSION['id'];
-        $this->name = $_SESSION['name'];
-        $this->admin = $_SESSION['admin'];
-        $this->website = $_SESSION['website'];
-      }
-    } 
-
-    else if( !empty($_COOKIE[Config::$rememberCookie]) ) { // remember cookie found
-      $u = DB::getRow(
-        'SELECT id, name, admin, website FROM '.TABLE_USERS.' WHERE remember = :1', 
-        $_COOKIE[Config::$rememberCookie]
-      );
-      if( !empty($u) ) {
-
-        session_start();
-        $_SESSION['id'] = $this->id = $u['id'];
-        $_SESSION['name'] = $this->name = $u['name'];
-        $_SESSION['admin'] = $this->admin = $u['admin'];
-        $_SESSION['website'] = $this->website = $u['website'];
-
-        // refresh for another year
-        setcookie( Config::$rememberCookie, $_COOKIE[Config::$rememberCookie], time() + 3600 * 24 * 365, Config::$absolutePath );
-
-        if( Config::$vbbIntegration['enabled'] ) { 
-          global $vbulletin;
-          $forum = new ForumOps($vbulletin);
-          $forum->login(array('username' => $u['name']));
-        }
-      }
-    }
+    return isset($_POST['login']) ?
+      $this->login_password() :
+      ($this->login_session() || $this->login_remember());
   }
+
+
+  private function login_password()
+  {
+    if (empty($_POST['name']) || empty($_POST['pass']))
+      return false;
+
+    $this->name = $_POST['name'];
+    $password = $_POST['pass'];
+
+    $r = DB::query_nofetch(
+      'SELECT `id`, `admin`, `website`, `email`, `pass` AS `passwordHash`
+       FROM ' . DB::escape_identifier(TABLE_USERS) .
+      'WHERE `name` = ? AND `valid`',
+      array($this->name),
+      array(PDO::FETCH_INTO, &$this));
+    $success = $r->fetch() !== false;
+    $r->closeCursor();
+    unset($r);
+
+    $success = $success && $this->check_password($password);
+
+    if ($success) {
+      session_start();
+      $this->__to_array($_SESSION);
+
+      if (filter_input(INPUT_POST, 'remember', FILTER_VALIDATE_BOOLEAN))
+        $this->update_remember();
+
+      if( Config::$vbbIntegration['enabled'] ) {
+        global $vbulletin;
+        $forum = new ForumOps($vbulletin);
+        $forum->login(array('username' => $this->name));
+      }
+    }
+    else {
+      $this->reset();
+    }
+
+    return $success;
+  }
+
+
+  private function login_session()
+  {
+    if (empty($_COOKIE[Config::$sessionCookie]))
+      return false;
+
+    // session running
+    session_start();
+    $success = !empty($_SESSION['id']);
+    if ($success) {
+      $this->__from_array($_SESSION);
+    } else {
+      setcookie(Config::$sessionCookie, false, 1, Config::$absolutePath);
+    }
+    return $success;
+  }
+
+
+  private function login_remember()
+  {
+    if (empty($_COOKIE[Config::$rememberCookie]))
+      return false;
+
+    $this->validationString =
+      base64_decode($_COOKIE[Config::$rememberCookie], true);
+
+    if ($this->validationString === false) {
+      setcookie(Config::$sessionCookie, false, 1, Config::$absolutePath);
+      return false;
+    }
+
+    // remember cookie found
+    $r = DB::query_nofetch(
+      'SELECT `id`, `name`, `admin`, `website`, `email`
+       FROM ' . DB::escape_identifier(TABLE_USERS) .
+      'WHERE `remember` = ?',
+      array($this->validationString),
+      array(PDO::FETCH_INTO, &$this));
+    $success = $r->fetch() !== false;
+    $r->closeCursor();
+    unset($r);
+
+    if ($success) {
+      session_start();
+      $this->__to_array($_SESSION);
+
+      // refresh for another year
+      $this->update_remember($_COOKIE[Config::$rememberCookie]);
+
+      if( Config::$vbbIntegration['enabled'] ) {
+        global $vbulletin;
+        $forum = new ForumOps($vbulletin);
+        $forum->login(array('username' => $this->name));
+      }
+    }
+
+    return $success;
+  }
+
+
+  private function check_password( $password )
+  {
+    require_once('lib/password.php');
+
+    if (($this->passwordHash[0] === '$') ?
+      password_verify($password, $this->passwordHash) :
+      (md5($password) === $this->passwordHash))
+    {
+      if (password_needs_rehash($this->passwordHash, PASSWORD_DEFAULT)) {
+        $this->passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        $r = DB::query_nofetch(
+          'UPDATE ' . DB::escape_identifier(TABLE_USERS) .
+          'SET `pass` = ? WHERE `id` = ?',
+          $this->passwordHash, $this->id);
+        assert($r && $r->rowCount() === 1);
+      }
+      return true;
+    }
+    return false;
+  }
+
+
+  private function update_remember()
+  {
+    $this->validationString = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
+    $this->update_remember_db();
+    $this->update_remember_cookie();
+  }
+
+  private function update_remember_db()
+  {
+    $r = DB::query_nofetch(
+      'UPDATE ' . DB::escape_identifier(TABLE_USERS) .
+      'SET `remember` = ? WHERE `id` = ?',
+      $this->validationString, $this->id);
+    assert($r->rowCount() === 1);
+  }
+
+  private function update_remember_cookie( $remember = null )
+  {
+    if (is_null($remember))
+      $remember = base64_encode($this->validationString);
+
+    setcookie(Config::$rememberCookie, $remember,
+      time() + 3600 * 24 * 365, Config::$absolutePath);
+  }
+
 
   public function profile( $localFile, &$messages ) {
     $upd = array( 'website' => $_POST['website'] );
 
     $_SESSION['website'] = $_POST['website'];
 
-    if( Config::$vbbIntegration['enabled'] ) { 
+    if( Config::$vbbIntegration['enabled'] ) {
       global $vbulletin;
       $forum = new ForumOps($vbulletin);
     }
@@ -159,7 +280,7 @@ class User {
       }
       else {
         $upd['pass'] = md5($_POST['cpass2']);
-        if( Config::$vbbIntegration['enabled'] ) { 
+        if( Config::$vbbIntegration['enabled'] ) {
           $forum->set_pass( $_SESSION['name'], $_POST['cpass2']);
         }
       }
@@ -175,10 +296,10 @@ class User {
       }
     }
 
-    if( empty($this->email) && 
-      preg_match('/^[\.\w\-\+]{1,}@[\.\w\-]{2,}\.[\w]{2,}$/', $_POST['email']) 
+    if( empty($this->email) &&
+      preg_match('/^[\.\w\-\+]{1,}@[\.\w\-]{2,}\.[\w]{2,}$/', $_POST['email'])
     ) {
-      if( Config::$vbbIntegration['enabled'] ) { 
+      if( Config::$vbbIntegration['enabled'] ) {
         $forum->set_email( $_SESSION['name'], $_POST['email']);
       }
       $upd['email'] = $_POST['email'];
@@ -187,35 +308,24 @@ class User {
     DB::updateRow( TABLE_USERS, array( 'id' => $this->id ), $upd );
   }
 
-  public function loadEmail() {
-    $tmp = DB::getRow( 
-      'SELECT email FROM '.TABLE_USERS.' WHERE id = :1',
-      $this->id
-    );
-    $this->email = $tmp['email'];
-  }
 
   public function logout() {
     session_unset();
     session_destroy();
     $_SESSION = array();
-    setcookie( Config::$rememberCookie, false, time() - 3600, Config::$absolutePath );
-    setcookie( Config::$sessionCookie, false, time() - 3600, Config::$absolutePath );
 
-    if( Config::$vbbIntegration['enabled'] ) { 
+    $this->validationString = null;
+    setcookie(Config::$rememberCookie, false, 1, Config::$absolutePath);
+    setcookie(Config::$sessionCookie, false, 1, Config::$absolutePath);
+    $this->update_remember_db();
+
+    if( Config::$vbbIntegration['enabled'] ) {
       global $vbulletin;
       $forum = new ForumOps($vbulletin);
       $forum->logout();
     }
   }
 
-  public function increaseScore( $amount ) {
-    DB::query( 
-      'UPDATE '.TABLE_USERS.' SET score = score + :1 WHERE id = :2', 
-      intval( $amount ),
-      $this->id
-    );
-  }
 
   public function register( &$messages ) {
 
@@ -268,7 +378,7 @@ class User {
       'email' => $_POST['email']
     ));
 
-    if( Config::$vbbIntegration['enabled'] ) { 
+    if( Config::$vbbIntegration['enabled'] ) {
       global $vbulletin;
       $forum = new ForumOps($vbulletin);
       $user['name'] = $_POST['name'];
