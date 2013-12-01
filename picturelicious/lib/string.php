ae03d49cdfc7d78218955860edec57d1ff264f6a
dekaden|Z <github.dekadenz@xoxy.net>
2013-12-01 17:05:06 +0100

    si_size: handle invalid values better, guess base

--- picturelicious/lib/string.php
+++ picturelicious/lib/string.php
@@ -1,4 +1,6 @@
 <?php
+include_once('lib/numeric.php');
+
 
 define('CRLF', "\r\n");
 
@@ -7,26 +9,32 @@ define('SI_PREFIXES', 'kMGTPEY');
 
 function si_size($value, $unit = NULL, $digits = 2, $base = 1000)
 {
+  if ($base === 'guess')
+    $base = (is_int($value) && is_powerof2(abs($value))) ? 1024 : 1000;
+
   assert($base == 1000 || $base == 1024);
-
   $log_base = 0;
-  if ($value != 0) {
-    $log_base_max = strlen(SI_PREFIXES);
-    while ($log_base <= $log_base_max && abs($value) >= $base) {
-      $value /= $base;
-      $log_base++;
-    }
 
-    $decimals = ($log_base || !is_int($value)) ?
-      max($digits - ((int) log10(abs($value))) - 1, 0) :
+  if ((!is_float($value) || is_finite($value)) && $value != 0)
+  {
+    $log_base_max = strlen(SI_PREFIXES);
+    for (; $log_base <= $log_base_max && abs($value) >= $base; ++$log_base)
+      $value /= $base;
+
+    $decimals = ($log_base !== 0 || $value != (int) $value) ?
+      max($digits - (int) log10(abs($value)) - 1, 0) :
       0;
-  } else {
+  }
+  else
+  {
     $decimals = 0;
+    if (is_null($value)) $value = NAN;
   }
 
   $rv = number_format($value, $decimals) . ' ';
 
-  if ($log_base != 0) {
+  if ($log_base !== 0)
+  {
     $prefixes = SI_PREFIXES;
     $rv .= $prefixes[$log_base-1];
     if ($base == 1024)
